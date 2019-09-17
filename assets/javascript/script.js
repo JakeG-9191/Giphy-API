dfd727255de7fb7c6afb472419e691088a68a272
Jake Garlick <jacobgarlick@msn.com>
2019-09-17 15:31:02 -0600

    started building out API connection for gifs, added initial functions to test rendering to page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,20 +1,58 @@
 var topics = [
-    "Game of Thrones", "Harry Potter", "Carnival Row", "Brothers Grimm"
+  "Game of Thrones", "Harry Potter", "Carnival Row", "Brothers Grimm"
 ];
 
 
-function displayGif(){
+function searchGif() {
 
-// var search = $(this).attr("data-name");
-var search = "harry potter"
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&q=" + search +"&limit=10&offset=10";
+  // var search = $(this).attr("data-name");
+  var search = "harry potter"
+  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&q=" + search + "&limit=10&offset=10";
 
-$.ajax({
+  $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(response) {
-    console.log(response);
+  }).then(function (response) {
+    var giphyData = response.data;
+    console.log(giphyData)
+    console.log(giphyData[0])
+    console.log(giphyData[0].rating)
+    console.log(giphyData[0].images.fixed_height_still.url)
+    console.log(giphyData[0].images.original.url)
+
+    for (var i = 0; i < topics.length; i++){
+      var newDiv = $("<div>");
+      var rating = $("<p>");
+
+      rating.text("Rating: " + giphyData[i].rating);
+
+      var holdStill = $("<img>");
+      holdStill.attr("src", giphyData[i].images.fixed_height_still.url);
+
+      newDiv.append(rating, holdStill);
+      $(".gifs").prepend(newDiv);
+    }
+
   });
 }
 
+function displayGif() {
+  // $(".gifs").empty();
+  
+  var gifState = $(this).attr("data-state");
+
+  var still = $(this).attr("data-still");
+  var animate = $(this).attr("data-animate");
+
+  if (gifState === "still"){
+    $(this).attr("src", animate)
+    $(this).attr("data-state", "animate")
+  } else {
+    $(this).attr("src", still)
+    $(this).attr("data-state", "still")
+  }
+
+}
+
+searchGif();
 displayGif();
