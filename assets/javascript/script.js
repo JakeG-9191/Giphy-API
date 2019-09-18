aec6dccd8e8ec5c59d838b6d421009eedd02aae4
Jake Garlick <jacobgarlick@msn.com>
2019-09-18 10:01:06 -0600

    added animate and still functionality for gifs, started working on search function for additional gifs per user input

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,12 +1,12 @@
 var topics = [
-  "Game of Thrones", "Harry Potter", "Carnival Row", "Brothers Grimm"
+  "Die Hard", "Mission Impossible", "Signs", "", ""
 ];
 
 
 function searchGif() {
 
   // var search = $(this).attr("data-name");
-  var search = "harry potter"
+  var search = "Die Hard"
   var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&q=" + search + "&limit=10&offset=10";
 
   $.ajax({
@@ -19,40 +19,46 @@ function searchGif() {
     console.log(giphyData[0].rating)
     console.log(giphyData[0].images.fixed_height_still.url)
     console.log(giphyData[0].images.original.url)
-
+    
     for (var i = 0; i < topics.length; i++){
       var newDiv = $("<div>");
       var rating = $("<p>");
-
+      
       rating.text("Rating: " + giphyData[i].rating);
-
+      
       var holdStill = $("<img>");
       holdStill.attr("src", giphyData[i].images.fixed_height_still.url);
+      holdStill.attr("data-still", giphyData[i].images.fixed_height_still.url);
+      holdStill.attr("data-animate", giphyData[i].images.original.url);
+      holdStill.attr("data-state", "still")
+      holdStill.attr("class", "gifs");
 
+      
       newDiv.append(rating, holdStill);
-      $(".gifs").prepend(newDiv);
+      $(".holder").prepend(newDiv);
     }
-
+    
+    $(".gifs").on("click", function() {
+      var gifState = $(this).attr("data-state");
+      var still = $(this).attr("data-still");
+      var animate = $(this).attr("data-animate");
+      
+      if (gifState === "still"){
+        $(this).attr("src", animate)
+        $(this).attr("data-state", "animate")
+      } else {
+        $(this).attr("src", still)
+        $(this).attr("data-state", "still")
+      }
+      
+      console.log(gifState)
+    });
   });
 }
 
-function displayGif() {
-  // $(".gifs").empty();
-  
-  var gifState = $(this).attr("data-state");
-
-  var still = $(this).attr("data-still");
-  var animate = $(this).attr("data-animate");
-
-  if (gifState === "still"){
-    $(this).attr("src", animate)
-    $(this).attr("data-state", "animate")
-  } else {
-    $(this).attr("src", still)
-    $(this).attr("data-state", "still")
-  }
+for (var i = 0; i < topics.length; i++){
 
 }
 
+
 searchGif();
-displayGif();
